c2e748262a47003fdd7eca9421a3db4e3715a4f8
Mitchell Lisle <mitchelllisle@mailbox.org>
2024-12-03 19:55:08 +1100

    add day three

--- src/main/scala/io/github/mitchelllisle/Main.scala
+++ src/main/scala/io/github/mitchelllisle/Main.scala
@@ -2,7 +2,7 @@ package io.github.mitchelllisle
 
 import io.github.mitchelllisle.DayOne.{DayOnePartOne, DayOnePartTwo}
 import io.github.mitchelllisle.DayTwo.{DayTwoPartOne, DayTwoPartTwo}
-import io.github.mitchelllisle.DayThree.DayThreePartOne
+import io.github.mitchelllisle.DayThree.{DayThreePartOne, DayThreePartTwo}
 
 
 object Main {
@@ -12,7 +12,8 @@ object Main {
       new DayOnePartTwo("src/main/resources/DayOne.txt"),
       new DayTwoPartOne("src/main/resources/DayTwo.txt"),
       new DayTwoPartTwo("src/main/resources/DayTwo.txt"),
-      new DayThreePartOne("src/main/resources/DayThree.txt")
+      new DayThreePartOne("src/main/resources/DayThree.txt"),
+      new DayThreePartTwo("src/main/resources/DayThree.txt")
     )
     solutions.foreach(_.printSolution())
   }
--- src/test/scala/TestDayThree.scala
+++ src/test/scala/TestDayThree.scala
@@ -1,9 +1,14 @@
-import io.github.mitchelllisle.DayThree.DayThreePartOne
+import io.github.mitchelllisle.DayThree.{DayThreePartOne, DayThreePartTwo}
 import org.scalatest.flatspec.AnyFlatSpec
 
 class TestDayThree extends AnyFlatSpec {
   "DayThreePartOne" should "produce correct output" in {
-    val solution = new DayThreePartOne("src/test/resources/DayThree.txt").solve()
+    val solution = new DayThreePartOne("src/test/resources/DayThreePartOne.txt").solve()
     assert(solution == 161)
+  }
+
+  "DayThreePartTwo" should "produce correct output" in {
+    val solution = new DayThreePartTwo("src/test/resources/DayThreePartTwo.txt").solve()
+    assert(solution == 48)
   }
 }
